6acf56c0f34e264008dd76213324ffd016788480
sam <samgoyal0044@gmail.com>
2024-08-13 15:34:47 -0700

    changed the partition key path for chat participants container and removed specialization container

--- azure/App/cosmosdb/main.tf
+++ azure/App/cosmosdb/main.tf
@@ -72,16 +72,6 @@ resource "azurerm_cosmosdb_sql_container" "chat_participants" {
   resource_group_name   = azurerm_resource_group.example.name
   account_name          = azurerm_cosmosdb_account.example.name
   database_name         = azurerm_cosmosdb_sql_database.main.name
-  partition_key_path    = "/userId"
-  partition_key_version = 2
-  throughput            = var.throughput
-}
-
-resource "azurerm_cosmosdb_sql_container" "chat_specialization_sessions" {
-  name                  = "chatspecializationsessions"
-  resource_group_name   = azurerm_resource_group.example.name
-  account_name          = azurerm_cosmosdb_account.example.name
-  database_name         = azurerm_cosmosdb_sql_database.main.name
   partition_key_path    = "/chatId"
   partition_key_version = 2
   throughput            = var.throughput
