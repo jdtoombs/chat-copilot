3e7dcbbbd1c7752c14ceadeb0e37e56c558c31e2
Hanson Chun <hanson.chun@quartech.com>
2024-10-09 11:45:44 -0700

    Remove state hook from toast offset

--- webapp/src/components/shared/Alerts.tsx
+++ webapp/src/components/shared/Alerts.tsx
@@ -11,7 +11,7 @@ import {
     useToastController,
 } from '@fluentui/react-components';
 import { Dismiss12Regular } from '@fluentui/react-icons';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AlertType } from '../../libs/models/AlertType';
 import { Environment } from '../../libs/services/BaseService';
 import { useAppDispatch, useAppSelector } from '../../redux/app/hooks';
@@ -30,8 +30,8 @@ const Alerts = () => {
     const dispatch = useAppDispatch();
     const toasterId = useId('toaster');
     const { dispatchToast } = useToastController(toasterId);
-    const [horizontal] = React.useState(20);
-    const [vertical] = React.useState(48);
+    const horizontalOffset = 20;
+    const verticalOffset = 48;
 
     const { alerts } = useAppSelector((state: RootState) => state.app);
     const [activeToasts, setActiveToasts] = useState(new Set<string | undefined>());
@@ -80,7 +80,7 @@ const Alerts = () => {
         });
     }, [alerts, dispatch, dispatchToast, activeToasts]);
 
-    return <Toaster toasterId={toasterId} offset={{ horizontal, vertical }} />;
+    return <Toaster toasterId={toasterId} offset={{ horizontal: horizontalOffset, vertical: verticalOffset }} />;
 };
 
 export default Alerts;
