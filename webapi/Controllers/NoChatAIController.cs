2eaa1dac491df39ca840a64c240d6fda48b18baf
Lenner <grayson.lenner@quartech.com>
2025-01-15 17:39:28 -0800

    now getting response from kernel.. clean up needed

--- webapi/Controllers/NoChatAIController.cs
+++ webapi/Controllers/NoChatAIController.cs
@@ -13,6 +13,61 @@ using Microsoft.Extensions.Options;
 
 namespace CopilotChat.WebApi.Controllers;
 
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.Security.KeyVault.Secrets;
+using CopilotChat.WebApi.Models.Request;
+using CopilotChat.WebApi.Models.Response;
+using CopilotChat.WebApi.Plugins.Chat.Ext;
+using CopilotChat.WebApi.Storage;
+// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using CopilotChat.WebApi.Auth;
+using CopilotChat.WebApi.Hubs;
+using CopilotChat.WebApi.Models.Request;
+using CopilotChat.WebApi.Models.Response;
+using CopilotChat.WebApi.Models.Storage;
+using CopilotChat.WebApi.Options;
+using CopilotChat.WebApi.Plugins.Chat;
+using CopilotChat.WebApi.Services;
+using CopilotChat.WebApi.Storage;
+using CopilotChat.WebApi.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Graph;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.MsGraph;
+using Microsoft.SemanticKernel.Plugins.MsGraph.Connectors;
+using Microsoft.SemanticKernel.Plugins.MsGraph.Connectors.Client;
+using Microsoft.SemanticKernel.Plugins.OpenApi;
+using Microsoft.KernelMemory;
+
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.TextToImage;
+using OpenAI.Chat;
+
 /// <summary>
 /// Controller responsible for handling chat messages and responses.
 /// </summary>
@@ -56,12 +111,13 @@ public class SessionlessChatController : ControllerBase
     [ProducesResponseType(StatusCodes.Status504GatewayTimeout)]
     //[Authorize(Policy = AuthPolicyName.RequireSpecialization)]
     public async Task<IActionResult> SessionlessChatAsync(
-       // [FromServices] Kernel kernel,
-        [FromBody] Ask ask
+        [FromServices] Kernel kernel,
+        [FromBody] Ask ask,
+        CancellationToken cancellationToken
     )
     {
 
-        var Test = this._singleMessageCompletionService.GetResponse();
+        var Test = this._singleMessageCompletionService.GetResponse(ask.Input, kernel, cancellationToken);
         // KernelFunction? chatFunction = kernel.Plugins.GetFunction(ChatPluginName, ChatFunctionName);
         return this.Ok(new { Success = true, Response = Test });
     }
--- webapi/Services/SingleMessageCompletionService.cs
+++ webapi/Services/SingleMessageCompletionService.cs
@@ -9,6 +9,54 @@ using CopilotChat.WebApi.Models.Request;
 using CopilotChat.WebApi.Models.Response;
 using CopilotChat.WebApi.Plugins.Chat.Ext;
 using CopilotChat.WebApi.Storage;
+// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using CopilotChat.WebApi.Auth;
+using CopilotChat.WebApi.Hubs;
+using CopilotChat.WebApi.Models.Request;
+using CopilotChat.WebApi.Models.Response;
+using CopilotChat.WebApi.Models.Storage;
+using CopilotChat.WebApi.Options;
+using CopilotChat.WebApi.Plugins.Chat;
+using CopilotChat.WebApi.Services;
+using CopilotChat.WebApi.Storage;
+using CopilotChat.WebApi.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Graph;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.MsGraph;
+using Microsoft.SemanticKernel.Plugins.MsGraph.Connectors;
+using Microsoft.SemanticKernel.Plugins.MsGraph.Connectors.Client;
+using Microsoft.SemanticKernel.Plugins.OpenApi;
+using Microsoft.KernelMemory;
+
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.TextToImage;
+using OpenAI.Chat;
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+
 
 namespace CopilotChat.WebApi.Services;
 
@@ -42,12 +90,27 @@ public class SingleMessageCompletionService
     /// <summary>
     /// Retrieves the search results using AzureAISearch service.
     /// </summary>
-    public async Task<string> GetResponse()
+    public async Task<string> GetResponse(
+        string userPrompt,
+        Kernel kernel,
+        CancellationToken cancellationToken
+    )
     {
+
+        var chatCompletion = kernel.GetRequiredService<IChatCompletionService>();
+        var stream = await chatCompletion.GetChatMessageContentAsync(
+            userPrompt,
+            null, // null because we currently do not use specialization data to generate suggestions.
+            kernel,
+            cancellationToken
+        );
+
         // Dummy implementation to avoid failure
-        await Task.Delay(500); // Simulate an async operation, e.g., API call, database access, etc.
-        return "Dummy response from GetResponse method.";
+        //await Task.Delay(500); // Simulate an async operation, e.g., API call, database access, etc.
+        return stream?.Content ?? "no response!";
     }
+
+
 
 
 }
