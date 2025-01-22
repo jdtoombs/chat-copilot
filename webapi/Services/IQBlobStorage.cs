ada138a1e63073c09d2bb033b2cfad10380c988e
Matthew Thiessen <matt.thiessen.dev@gmail.com>
2025-01-22 08:34:34 -0800

    feat: create IQBlobStorage interface

---
+++ webapi/Services/IQBlobStorage.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace CopilotChat.WebApi.Services;
+
+public interface IQBlobStorage
+{
+    Task<bool> BlobExistsAsync(System.Uri blobURI);
+
+    Task<string> AddBlobAsync(IFormFile blob);
+
+    Task DeleteBlobByURIAsync(System.Uri blobURI);
+}
