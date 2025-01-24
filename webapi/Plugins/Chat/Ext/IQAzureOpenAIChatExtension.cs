81019ab8c2c8d0a18bc0b1a7b4704f9f72bcb7e8
Matthew Thiessen <matt.thiessen.dev@gmail.com>
2025-01-24 12:06:43 -0800

    feat: create IQAzureOpenAIChatExtension

---
+++ webapi/Plugins/Chat/Ext/IQAzureOpenAIChatExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Azure.AI.OpenAI.Chat;
+using CopilotChat.WebApi.Models.Storage;
+
+namespace CopilotChat.WebApi.Plugins.Chat.Ext;
+
+#pragma warning disable AOAI001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+public interface IQAzureOpenAIChatExtension
+{
+    Task<AzureSearchChatDataSource?> GetAzureSearchChatDataSource(Specialization? specialization);
+    Task<(string? indexName, string? ApiKey, string? Endpoint)> GetAISearchDeploymentConnectionDetails(string indexId);
+}
