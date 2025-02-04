149b96c04bc87615b4be4ab82c14a07331865269
mattthiessendev <matt.thiessen.dev@gmail.com>
2025-02-04 07:34:33 -0800

    style [AB#62]: remove dead code QAzureOpenAIChatOptions (#139)

--- webapi/Plugins/Chat/Ext/QAzureOpenAIChatOptions.cs
+++ webapi/Plugins/Chat/Ext/QAzureOpenAIChatOptions.cs
@@ -1,7 +1,5 @@
 ﻿// Copyright (c) Quartech. All rights reserved.
 
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CopilotChat.WebApi.Plugins.Chat.Ext;
@@ -16,19 +14,9 @@ public class QAzureOpenAIChatOptions
     public string DefaultModel { get; set; } = "";
     public string DefaultEmbeddingModel { get; set; } = "";
     public string DefaultConnection { get; set; } = "";
-    public string DefaultSpecializationDescription { get; set; } = "";
     public string DefaultSpecializationImage { get; set; } = "";
     public string DefaultSpecializationIcon { get; set; } = "";
     public string AdminGroupMembershipId { get; set; } = "";
-    public bool DefaultRestrictResultScope { get; set; } = false;
-    public int DefaultStrictness { get; set; } = 0;
-    public int DefaultDocumentCount { get; set; } = 0;
-    public int DefaultPastMessagesIncludedCount { get; set; } = 0;
-    public int DefaultMaxResponseTokenLimit { get; set; } = 0;
-
-    // [Required]
-    // public IList<OpenAIDeploymentConnection> OpenAIDeploymentConnections { get; set; } =
-    //     new List<OpenAIDeploymentConnection>();
 
     [Required]
     public BlobStorageOption BlobStorage { get; set; } = new BlobStorageOption();
@@ -38,60 +26,6 @@ public class QAzureOpenAIChatOptions
         public string Name { get; set; } = string.Empty;
         public int CompletionTokenLimit { get; set; }
         public int OutputTokens { get; set; }
-    }
-
-    public class ImageCompletionDeployment
-    {
-        // may contain future settings
-        public string Name { get; set; } = string.Empty;
-    }
-
-    public class OpenAIDeploymentConnection
-    {
-        public string Name { get; set; } = string.Empty;
-        public Uri? Endpoint { get; set; } = null;
-        public string APIKey { get; set; } = string.Empty;
-        public IList<ChatCompletionDeployment> ChatCompletionDeployments { get; set; } =
-            new List<ChatCompletionDeployment>();
-        public IList<ImageCompletionDeployment> ImageGenerationDeployments { get; set; } =
-            new List<ImageCompletionDeployment>();
-        public IList<string> EmbeddingDeployments { get; set; } = new List<string>();
-    }
-
-    public class AITextToImageDeploymentConnection
-    {
-        public string Name { get; set; } = string.Empty;
-        public Uri? Endpoint { get; set; } = null;
-        public string APIKey { get; set; } = string.Empty;
-        public string Deployment { get; set; } = string.Empty;
-    }
-
-    public class AISearchDeploymentConnection
-    {
-        public string Name { get; set; } = string.Empty;
-        public Uri? Endpoint { get; set; } = null;
-        public string APIKey { get; set; } = string.Empty;
-    }
-
-    public class QSpecializationIndex
-    {
-        public string IndexName { get; set; } = string.Empty;
-        public string QueryType { get; set; } = string.Empty;
-        public string SemanticConfiguration { get; set; } = "default";
-        public bool RestrictResultScope { get; set; } = true;
-
-        public string AISearchDeploymentConnection { get; set; } = string.Empty;
-        public string OpenAIDeploymentConnection { get; set; } = string.Empty;
-        public string EmbeddingDeployment { get; set; } = string.Empty;
-        public FieldMappingOption? FieldMapping { get; set; } = new FieldMappingOption();
-    }
-
-    public class FieldMappingOption
-    {
-#pragma warning disable CA1056
-        public string UrlFieldName { get; set; } = "url";
-        public string TitleFieldName { get; set; } = "title";
-        public string FilepathFieldName { get; set; } = "filepath";
     }
 
     public class BlobStorageOption
