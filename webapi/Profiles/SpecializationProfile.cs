26bee3ccca9a5a7de74adc7dff2ab1d7ae62d1b7
mattthiessendev <matt.thiessen.dev@gmail.com>
2025-03-28 11:12:57 -0700

    refactor [AB#97]: Remove constructor param mapping (#314)

--- webapi/Profiles/SpecializationProfile.cs
+++ webapi/Profiles/SpecializationProfile.cs
@@ -16,7 +16,6 @@ public class SpecializationProfile : Profile
                 destination => destination.Name,
                 option => option.MapFrom(source => source.CompletionDeploymentName)
             )
-            .ForCtorParam("Name", option => option.MapFrom(source => source.CompletionDeploymentName))
             .ReverseMap();
 
         this.CreateMap<Specialization, SpecializationResponse>();
