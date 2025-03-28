4ed7e73cbfb64ab6a46674eb94a2a55a47813a5b
mattthiessendev <matt.thiessen.dev@gmail.com>
2025-03-28 09:39:07 -0700

    refactor [AB#97]: remove primary constructor from CompletionDeploymentModel (#313)

--- webapi/Models/Storage/CompletionDeploymentModel.cs
+++ webapi/Models/Storage/CompletionDeploymentModel.cs
@@ -2,9 +2,15 @@
 
 namespace CopilotChat.WebApi.Models.Storage;
 
-public record CompletionDeploymentModel(string OpenAIDeploymentId, string Name, string SpecializationId)
-    : IStorageEntity
+public record CompletionDeploymentModel : IStorageEntity
 {
     public required string Id { get; set; }
+
     public string Partition => this.SpecializationId;
+
+    public required string SpecializationId { get; set; }
+
+    public required string OpenAIDeploymentId { get; set; }
+
+    public required string Name { get; set; }
 }
